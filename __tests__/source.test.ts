055c943f982aa0f9cd8ff23ea62eb4b031298b88
Miguel Ceriani <miguel.ceriani@gmail.com>
2020-10-21 19:00:26 +0200

    Update test

--- __tests__/source.test.ts
+++ __tests__/source.test.ts
@@ -4,21 +4,40 @@ import { translate, toSparql  } from 'sparqlalgebrajs';
 test("Testing 'Source' structures", () => {
 
     const defaultSparqlEndpointDS = new S.DefaultSparqlEndpoint();
+    expect(defaultSparqlEndpointDS instanceof S.DefaultSparqlEndpoint).toBe(true);
+    expect(defaultSparqlEndpointDS).toEqual({consumers: []});
 
-    console.log(defaultSparqlEndpointDS);
 
     const allTermsTS = new S.AllTermsFromDatasetSource(defaultSparqlEndpointDS);
+    expect(allTermsTS instanceof S.AllTermsFromDatasetSource).toBe(true);
+    expect(allTermsTS).toEqual({
+        consumers: [],
+        observers: [],
+        datasetSource: defaultSparqlEndpointDS
+    });
+    expect(defaultSparqlEndpointDS).toHaveProperty('consumers', [allTermsTS]);
 
-    console.log(allTermsTS);
-
-    const selectQueryTxt = 'SELECT * WHERE { ?x ?y ?pippo. {SELECT * WHERE {?s ?p ?pippo} } }';
+    const selectQueryTxt = 'SELECT ?newThis WHERE { ?this ?p ?newThis }';
     const selectQuery = translate(selectQueryTxt);
 
-    const varFromSelectQuery = new S.VarFromSelectQuery("newThis", selectQuery, defaultSparqlEndpointDS, new Map<string, S.RDFTermSource>([["this", allTermsTS]]));
+    const varFromSelectQuery =
+            new S.VarFromSelectQuery(
+                    "newThis", selectQuery, defaultSparqlEndpointDS,
+                    new Map<string, S.RDFTermSource>([["this", allTermsTS]]));
 
-    console.log(varFromSelectQuery);
-
-    expect(varFromSelectQuery).toBe(expect.anything());
-
+    expect(varFromSelectQuery instanceof S.VarFromSelectQuery).toBe(true);
+    expect(varFromSelectQuery).toEqual({
+        consumers: [],
+        observers: [],
+        varName: 'newThis',
+        selectQuery: selectQuery,
+        datasetSource: defaultSparqlEndpointDS,
+        params: new Map<string, S.RDFTermSource>([["this", allTermsTS]])
+        // params: {
+        //     'this': allTermsTS
+        // }
+    });
+    expect(defaultSparqlEndpointDS).toHaveProperty('consumers', [allTermsTS, varFromSelectQuery]);
+    expect(allTermsTS).toHaveProperty('consumers', [varFromSelectQuery]);
 
 });
