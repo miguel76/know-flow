a0c6409896942f6f706b4ce0caa84479a74afe53
Miguel Ceriani <miguel.ceriani@gmail.com>
2021-10-09 12:19:52 +0200

    Fix Types export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import * as Tasks from './task';
+import * as Types from './task';
 import TaskFactory, {TaskFactoryOptions} from './taskFactory';
 import TaskBuilder from './taskBuilder';
 import {stringifyTask} from './utils';
 import TaskEngine from './taskEngine';
 
 export {
-    Tasks,
+    Types,
     TaskFactory, TaskFactoryOptions,
     stringifyTask, TaskEngine,
     TaskBuilder
